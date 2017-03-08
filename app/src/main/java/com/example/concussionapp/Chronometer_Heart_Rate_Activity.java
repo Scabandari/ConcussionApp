53306b847039577a64e62ca81e4b804af640f1e8
mpcharlton <mariepiercharlton@gmail.com>
2017-03-08 12:34:26 -0500

    Sending my code to ryan

---
+++ app/src/main/java/com/example/concussionapp/Chronometer_Heart_Rate_Activity.java
@@ -0,0 +1,58 @@
+package com.example.concussionapp;
+
+
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.os.SystemClock;
+        import android.view.View;
+        //import android.view.View.OnClickListener;
+        import android.widget.Button;
+        import android.widget.Chronometer;
+
+public class Chronometer_Heart_Rate_Activity extends Activity {
+
+    Chronometer mChronometer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chronometer__heart__rate_);
+
+        Intent intent = getIntent(); //get the intent from the mainActivity to link them
+        //int intValue=intent.getIntExtra("maxEditText", 0);
+
+        Button StartButton;
+        Button StopButton;
+        Button ResetButton;
+
+        mChronometer = (Chronometer) findViewById(R.id.chronometer);
+
+        StartButton= (Button) findViewById(R.id.start_Button);
+        StartButton.setOnClickListener(mStartListener);
+
+        StopButton= (Button) findViewById(R.id.stop_Button);
+        StopButton.setOnClickListener(mStopListener);
+
+        ResetButton = (Button) findViewById(R.id.reset_Button);
+        ResetButton.setOnClickListener(mResetListener);
+
+    }
+
+    View.OnClickListener mStartListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            mChronometer.start();
+        }
+    };
+    View.OnClickListener mStopListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            mChronometer.stop();
+        }
+    };
+    View.OnClickListener mResetListener = new View.OnClickListener()
+    {
+        public void onClick(View v) {mChronometer.setBase(SystemClock.elapsedRealtime());}
+    };
+
+
+}
--- app/src/main/java/com/example/concussionapp/Excercise_Setup_Activity.java
+++ app/src/main/java/com/example/concussionapp/Excercise_Setup_Activity.java
@@ -1,9 +1,18 @@
 package com.example.concussionapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class Excercise_Setup_Activity extends AppCompatActivity {
+
+    protected EditText maxEditText;
+    protected EditText minEditText;
+    protected Spinner timeSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +21,35 @@ public class Excercise_Setup_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_excercise__setup_);
 
+        //linking the editText on the user interface to the one created here
+        maxEditText = (EditText) findViewById(R.id.MaxEditText);
+        minEditText = (EditText) findViewById(R.id.MinEditText);
+        timeSpinner = (Spinner) findViewById(R.id.TimeSpinner);
+
+        Button done_setup_button;
+
+        //linking the button on the user interface to the one created here and making it listen
+        done_setup_button = (Button) findViewById(R.id.DoneSetupButton);
+        done_setup_button.setOnClickListener(OnClickSetupButton);
 
     }
-}
+
+    public void sendMessage() //function to send the user to the next activity
+    {
+        Intent intent = new Intent (this,Chronometer_Heart_Rate_Activity.class);
+        startActivity(intent);
+        //intent.putExtra("maxEditText", intValue);
+    }
+
+    private Button.OnClickListener OnClickSetupButton = new Button.OnClickListener()
+    {
+        @Override
+        public void onClick(View v) {
+            sendMessage();
+
+        }
+    };
+    //making the button listen for it to be clicked
+
+    }
+
