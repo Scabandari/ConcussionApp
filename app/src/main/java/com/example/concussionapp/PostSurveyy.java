325274631f8fc7a262061472e47bff6377d4c248
Subhi <subz42@live.co.uk>
2017-04-02 21:01:44 -0400

    New updates:
    *Deleted files:
     -YesPop.java along with its xml file
     -NonPop.java along with its xml file
    
    *Modification:
    -PostSurveyy.java now has AlertDialog to show the neccesary messages
    -tool_bar.xml was changed for a better look with red borders
    -AndroidManifest.xml changed with the elimination of the two activities meantioned above

--- app/src/main/java/com/example/concussionapp/PostSurveyy.java
+++ app/src/main/java/com/example/concussionapp/PostSurveyy.java
@@ -1,6 +1,8 @@
 package com.example.concussionapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,35 +19,9 @@ public class PostSurveyy extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_surveyy);
 
-        Button Oui;
-        Button Non;
         Button Capiche;
 
-        Oui = (Button) findViewById(R.id.Yes_Button);
-        Non = (Button) findViewById(R.id.No_Button);
         Capiche = (Button) findViewById(R.id.understand_button);
-
-
-        Oui.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-
-                startActivity(new Intent(PostSurveyy.this,YesPop.class));
-
-            }
-        });
-
-        Non.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                startActivity(new Intent (PostSurveyy.this,NonPop.class));
-
-            }
-        });
 
         Capiche.setOnClickListener(new View.OnClickListener()
         {
@@ -57,10 +33,36 @@ public class PostSurveyy extends AppCompatActivity
             }
         });
 
-
-
-
     }
+
+        public void YesAlert(View v)
+        {
+            AlertDialog.Builder yesAlert = new AlertDialog.Builder(this);
+            yesAlert.setMessage("As you know, to recover well and faster, you should not experience any acute symptoms during your recovery process, you need to have NO symptoms during a training at all time, if you do STOP IMMEDIATELY and rest. If symptoms persist consult a doctor or a physician. You also need to spend 24 hours without having any symptoms before the next training session. Since some symptoms have occurred, you must restart you recovery process, starting from step 1. \nFull Rest for 24 hours.")
+                    .setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).setTitle("Alert!").create();
+            yesAlert.show();
+
+        }
+
+        public void NoAlert(View v)
+        {
+            AlertDialog.Builder noAlert = new AlertDialog.Builder(this);
+            noAlert.setMessage("You are one step closer to finishing your recovery process. You can proceed to next step of recovery. Be caution for the next 24 hours as symptoms might reoccur. If so, consult your care provider before the next training session.")
+                .setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).setTitle("Congratulations!").create();
+        noAlert.show();
+
+        }
+
 
 
 
